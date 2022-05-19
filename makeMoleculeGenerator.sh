8adfe49738e66f8b7c004ed4a7a7981df001780d
Arron <ccck67@struik.dur.ac.uk>
2022-05-19 11:49:34 +0100

    fixedMolGen

--- makeMoleculeGenerator.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-rm finalSrc/*.o
-g++ -c -O3 -std=gnu++14 -o finalSrc/independentFunctions.o finalSrc/independentFunctions.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/point.o  finalSrc/point.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/polyHelix.o finalSrc/polyHelix.cpp 
-g++ -c -O3 -std=gnu++14 -o finalSrc/randomMolGen.o finalSrc/randomMolGen.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/ktlMoleculeRandom.o finalSrc/ktlMoleculeRandom.cpp 
-g++ -c -O3 -std=gnu++14 -o finalSrc/mainGlobalOptMultimerRanStart.o finalSrc/mainGlobalOptMultimerRanStart.cpp 
-g++ -O3-std=gnu++14 -o makeMolecule finalSrc/independentFunctions.o finalSrc/point.o finalSrc/polyHelix.o finalSrc/randomMolGen.o finalSrc/ktlMoleculeRandom.o finalSrc/mainGlobalOptMultimerRanStart.o 
