74784396e234fd76a1d6db6a27495d6ced149bb3
Arron <ccck67@struik.dur.ac.uk>
2022-05-19 10:51:27 +0100

    fixedMolGenerator

--- makeMoleculeGenerator.sh
+++ makeMoleculeGenerator.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 rm src/*.o
-g++ -c -O3 -std=gnu++14 -o src/independentFunctions.o src/independentFunctions.cpp
-g++ -c -O3 -std=gnu++14 -o src/point.o  src/point.cpp
-g++ -c -O3 -std=gnu++14 -o src/polyHelix.o src/polyHelix.cpp 
-g++ -c -O3 -std=gnu++14 -o src/randomMolGen.o src/randomMolGen.cpp
-g++ -c -O3 -std=gnu++14 -o src/ktlMoleculeRandom.o src/ktlMoleculeRandom.cpp 
-g++ -c -O3 -std=gnu++14 -o src/mainGlobalOptMultimerRanStart.o src/mainGlobalOptMultimerRanStart.cpp 
-g++ -O3-std=gnu++14 -o makeMolecule src/independentFunctions.o src/point.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/mainGlobalOptMultimerRanStart.o 
+g++ -c -O3 -std=gnu++14 -o finalSrc/independentFunctions.o finalSrc/independentFunctions.cpp
+g++ -c -O3 -std=gnu++14 -o finalSrc/point.o  finalSrc/point.cpp
+g++ -c -O3 -std=gnu++14 -o finalSrc/polyHelix.o finalSrc/polyHelix.cpp 
+g++ -c -O3 -std=gnu++14 -o finalSrc/randomMolGen.o finalSrc/randomMolGen.cpp
+g++ -c -O3 -std=gnu++14 -o finalSrc/ktlMoleculeRandom.o finalSrc/ktlMoleculeRandom.cpp 
+g++ -c -O3 -std=gnu++14 -o finalSrc/mainGlobalOptMultimerRanStart.o finalSrc/mainGlobalOptMultimerRanStart.cpp 
+g++ -O3-std=gnu++14 -o makeMolecule finalSrc/independentFunctions.o finalSrc/point.o finalSrc/polyHelix.o finalSrc/randomMolGen.o finalSrc/ktlMoleculeRandom.o finalSrc/mainGlobalOptMultimerRanStart.o 
